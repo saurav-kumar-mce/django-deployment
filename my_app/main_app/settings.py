1a584845705711315e739655e4bf10a619eb541d
saurav-kr <saurav.kumar@essentia.dev>
2023-03-09 14:33:33 +0530

    tree

--- my_app/main_app/settings.py
+++ my_app/main_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-me@_$m299(eegpvtt%*6hps-cb@tr8rvs^a5c*&lw-!n2=wi8+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
